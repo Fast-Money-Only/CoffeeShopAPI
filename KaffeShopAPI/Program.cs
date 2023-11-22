0cf81f1a3a969f5cdb8d49e8a9f035ec4853b36b
m4gn3s1um <madmedmig@gmail.com>
2023-11-22 11:41:53 +0100

    Commit : expect succes

--- KaffeShopAPI/Program.cs
+++ KaffeShopAPI/Program.cs
@@ -13,10 +13,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-askaksldlkajsd
-asdnasdjkashdkjhasd
-    
-    sadajsd
+
 app.UseHttpsRedirection();
 
 var summaries = new[]
