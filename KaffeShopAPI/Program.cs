4e1860c702db733b5e91dd22397d558654d2b486
m4gn3s1um <madmedmig@gmail.com>
2023-11-22 12:11:58 +0100

    Testing

--- KaffeShopAPI/Program.cs
+++ KaffeShopAPI/Program.cs
@@ -16,8 +16,6 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
-asdasdasdasdasdasd
-
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
