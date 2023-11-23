c405461059922bc07399f635cfd6973bc77647a8
pernillepersson <pernillekp@me.com>
2023-11-23 09:31:36 +0100

    Ny test

--- KaffeShopAPI/Program.cs
+++ KaffeShopAPI/Program.cs
@@ -18,7 +18,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Magnus", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing", "Bracing", "Magnus", "Ændring", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
 app.MapGet("/weatherforecast", () =>
