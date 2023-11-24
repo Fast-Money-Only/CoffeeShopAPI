134b6542616d1d16822b868b56221cbc31fc5ea3
pernillepersson <pernillekp@me.com>
2023-11-24 12:21:40 +0100

    Membership implemented

--- _Data/Repository/MembershipRepository.cs
+++ _Data/Repository/MembershipRepository.cs
@@ -4,8 +4,15 @@ namespace _Data.Repository;
 
 public class MembershipRepository : IMembershipRepository
 {
+    private readonly CoffeeShopContext _context;
+
+    public MembershipRepository(CoffeeShopContext context)
+    {
+        _context = context;
+    }
+
     public Membership GetMemberhip(Guid id)
     {
-        throw new NotImplementedException();
+        return _context.Memberships.Find(id);
     }
 }
