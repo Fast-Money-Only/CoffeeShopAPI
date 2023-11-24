66a3c311112f61f5145a816b6c5f8ed7b7aebed1
pernillepersson <pernillekp@me.com>
2023-11-24 12:08:35 +0100

    Funktions implemented

--- _Data/Repository/ProductRepository.cs
+++ _Data/Repository/ProductRepository.cs
@@ -4,23 +4,33 @@ namespace _Data.Repository;
 
 public class ProductRepository : IProductRepository
 {
+    private readonly CoffeeShopContext _context;
+
+    public ProductRepository(CoffeeShopContext context)
+    {
+        _context = context;
+    }
+
     public IList<Product> GetProducts()
     {
-        throw new NotImplementedException();
+        return _context.Products.ToList();
     }
 
     public Product AddProduct(Product product)
     {
-        throw new NotImplementedException();
+        _context.Products.Add(product);
+        _context.SaveChanges();
+        return product;
     }
 
     public Product GetProduct(Guid id)
     {
-        throw new NotImplementedException();
+        return _context.Products.Find(id);
     }
 
     public void DeleteProduct(Guid id)
     {
-        throw new NotImplementedException();
+        _context.Products.Remove(GetProduct(id));
+        _context.SaveChanges();
     }
 }
