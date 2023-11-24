b842b6983e8e5b3d82866d84656139ffc8a9fab8
pernillepersson <pernillekp@me.com>
2023-11-24 10:58:12 +0100

    Delete funktion added

--- _Data/Repository/IProductRepository.cs
+++ _Data/Repository/IProductRepository.cs
@@ -7,5 +7,5 @@ public interface IProductRepository
     IList<Product> GetProducts();
     Product AddProduct(Product product);
     Product GetProduct(Guid id);
-    
+    void DeleteProduct(Guid id);
 }
