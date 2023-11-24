e0ed2df6d4cf929906dfe6fd627ee6b462c19534
pernillepersson <pernillekp@me.com>
2023-11-24 10:48:00 +0100

    Interface Product Repository

---
+++ _Data/Repository/IProductRepository.cs
@@ -0,0 +1,11 @@
+using Model;
+
+namespace _Data.Repository;
+
+public interface IProductRepository
+{
+    IList<Product> GetProducts();
+    Product AddProduct(Product product);
+    Product GetProduct(Guid id);
+    
+}
