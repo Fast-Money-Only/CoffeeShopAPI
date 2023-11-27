d9e2f19c327db66674f170041b6bfebb30aedf11
m4gn3s1um <madmedmig@gmail.com>
2023-11-27 12:24:33 +0100

    Added the last controllers

--- Presentation/Controllers/MembershipController.cs
+++ Presentation/Controllers/MembershipController.cs
@@ -1,6 +1,25 @@
+using Business.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
 namespace Presentation.Controllers;
 
-public class MembershipController
+[ApiController]
+[Route("api/[controller]")]
+public class MembershipController : Controller
 {
+    private readonly IMembershipService _membershipService;
     
+    public MembershipController(IMembershipService membershipService)
+    {
+        _membershipService = membershipService;
+    }
+    
+    [HttpGet]
+    [Route("GetMembership")]
+    public IActionResult GetMembership(Guid id)
+    {
+        var membership = _membershipService.GetMemberhip(id);
+        return Ok(membership);
+    }
 }
