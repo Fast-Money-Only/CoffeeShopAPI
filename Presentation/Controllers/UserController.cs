a6f17e0b5eeb32a8493b8555a59cba6889349037
m4gn3s1um <madmedmig@gmail.com>
2023-11-27 11:20:01 +0100

    Edited UserController.cs

--- Presentation/Controllers/UserController.cs
+++ Presentation/Controllers/UserController.cs
@@ -1,6 +1,78 @@
-﻿namespace Presentation.Controllers;
+﻿using Business.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Model;
 
-public class UserController
+namespace Presentation.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : Controller
 {
+    private readonly IUserService _userService;
     
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+    
+    [HttpGet]
+    [Route("GetUsers")]
+    public IActionResult GetUsers()
+    {
+        var users = _userService.GetUsers();
+        return Ok(users);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetUser(Guid id)
+    {
+        var user = _userService.GetUser(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return Ok(user);
+    }
+
+    [HttpPost]
+    public IActionResult CreateUser([FromBody] User user)
+    {
+        var newUser = _userService.CreateUser(user);
+        return CreatedAtAction(nameof(GetUser), new { id = newUser.Id }, newUser);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateUser(Guid id, [FromBody] User user)
+    {
+        try
+        {
+            var updatedUser = _userService.UpdateUser(id, user);
+            return Ok(updatedUser);
+        }
+        catch (Exception e)
+        {
+            return NotFound();
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteUser(Guid id)
+    {
+        try
+        {
+            _userService.DeleteUser(id);
+            return NoContent();
+        }
+        catch (Exception e)
+        {
+            return NotFound();
+        }
+    }
+    
+    [HttpGet("{id}")]
+    public IActionResult GetUserOrders(Guid id)
+    {
+        var userOrders = _userService.GetUserOrders(id);
+        return Ok(userOrders);
+    }
 }
